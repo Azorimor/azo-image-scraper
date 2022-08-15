3b61273b3b001e2bc5ba662ee7525706b76c2155
Azorimor <azorimor@gmail.com>
2022-08-15 12:47:02 +0200

    test embeding static files
    Signed-off-by: Azorimor <azorimor@gmail.com>

--- backend/server.go
+++ backend/server.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"azorimor/azo-image-scraper/database"
+	"embed"
+
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
+
+// Embed the frontend application
+//
+//go:embed ui/*
+var frontendApplication embed.FS
 
 func main() {
 	dbSetup()
 
 	app := fiber.New()
 	app.Use(logger.New())
+
+	app.Use("/ui", filesystem.New(filesystem.Config{
+		Root:       http.FS(frontendApplication),
+		PathPrefix: "ui",
+		Browse:     true,
+	}))
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
